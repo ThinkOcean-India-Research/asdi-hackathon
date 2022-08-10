c1ee72acf5147646f40c3601868eca61afbcc925
RohanGautam <rohan1gautam@gmail.com>
2022-08-10 11:21:39 +0530

    training logic

--- building-detection/training.py
+++ building-detection/training.py
@@ -115,28 +115,62 @@ def get_transform(train):
     return T.Compose(transforms)
 
 
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-dataset = BuildingFootprintDataset(get_transform(train=True))
+def main():
+    # train on the GPU or on the CPU, if a GPU is not available
+    device = torch.device(
+        'cuda') if torch.cuda.is_available() else torch.device('cpu')
+    # device = torch.device('cpu')
 
-# img, target = dataset[0]
-# print(img.shape, target['masks'].shape)
-# img, target = dataset[1200]
-# print(img.shape, target['masks'].shape)
-# print(dataset[1200])
+    # our dataset has two classes only - background and person
+    num_classes = 2
+    # use our dataset and defined transformations
+    dataset = BuildingFootprintDataset(get_transform(train=True))
+    dataset_test = BuildingFootprintDataset(get_transform(train=False))
+
+    # split the dataset in train and test set
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=2, shuffle=True, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+    # get the model using our helper function
+    model = get_model_instance_segmentation(num_classes)
+
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    # let's train it for 10 epochs
+    num_epochs = 10
+
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, data_loader,
+                        device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, data_loader_test, device=device)
+
+    print("That's it!")
 
 
-data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=2, shuffle=True, num_workers=4,
-    collate_fn=utils.collate_fn
-)
-
-
-images, targets = next(iter(data_loader))
-images = list(image for image in images)
-# print(images[0].shape)
-targets = [{k: v for k, v in t.items()} for t in targets]
-output = model(images, targets)   # Returns losses and detections
-# # For inference
-model.eval()
-x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
-predictions = model(x)           # Returns predictions
+if __name__ == "__main__":
+    torch.cuda.empty_cache()
+    main()
